014a0e978f7b624ce23477f1563cca0ca853f96f
RedMuri <celovekda@gmail.com>
2023-03-05 10:16:40 +0300

    made fragment_profile layout

---
+++ app/src/main/java/com/example/redmuriapp/ProfileFragment.kt
@@ -0,0 +1,28 @@
+package com.example.redmuriapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+
+class ProfileFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    companion object {
+
+        fun newInstance() =
+            ProfileFragment()
+    }
+}
