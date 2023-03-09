1d39b695310d745bf4b6c30e3a24be6a01f3e9d0
RedMuri <celovekda@gmail.com>
2023-03-09 10:57:37 +0300

    added binding

--- app/src/main/java/com/example/redmuriapp/signin/SignInFragment.kt
+++ app/src/main/java/com/example/redmuriapp/signin/SignInFragment.kt
@@ -8,26 +8,28 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.redmuriapp.R
+import com.example.redmuriapp.databinding.FragmentSignInBinding
 
 
 class SignInFragment : Fragment() {
 
-    private val signInViewModel = ViewModelProvider(requireActivity())[SignInViewModel::class.java]
+    private var _binding: FragmentSignInBinding? = null
+    private val binding: FragmentSignInBinding
+        get() = _binding ?: throw RuntimeException("FragmentSignInBinding == null")
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private val signInViewModel = ViewModelProvider(requireActivity())[SignInViewModel::class.java]
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+    ): View{
+        _binding = FragmentSignInBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        observeViewModel()
     }
 
     fun observeViewModel() {
@@ -47,7 +49,12 @@ class SignInFragment : Fragment() {
     }
 
     fun showError(errorCode: Int){
-        
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
