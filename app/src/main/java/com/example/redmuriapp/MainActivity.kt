aab1d91c02146d7a928e9810e14d48045bb360c6
RedMuri <celovekda@gmail.com>
2023-03-09 11:32:36 +0300

    added shared preferences

--- app/src/main/java/com/example/redmuriapp/MainActivity.kt
+++ app/src/main/java/com/example/redmuriapp/MainActivity.kt
@@ -2,11 +2,12 @@ package com.example.redmuriapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
+import android.text.Layout
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import androidx.preference.PreferenceManager
+import com.example.redmuriapp.fragments.MainFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -15,8 +16,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        checkUserSigned()
         setupBottomNavigation()
+    }
+
+    private fun checkUserSigned() {
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val isRegistered = pref.getBoolean("isRegistered",false)
+        if (!isRegistered){
+            findNavController(androidx.navigation.fragment.R.id.nav_host_fragment_container)
+                .navigate(R.id.signInFragment)
+        }
     }
 
     private fun setupBottomNavigation() {
--- app/src/main/java/com/example/redmuriapp/signin/SignInFragment.kt
+++ app/src/main/java/com/example/redmuriapp/signin/SignInFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.redmuriapp.R
 import com.example.redmuriapp.databinding.FragmentSignInBinding
 
 
