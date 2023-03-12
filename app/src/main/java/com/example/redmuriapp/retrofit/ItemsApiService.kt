5c07352c0ec362079a2dcfed1baa288832be76a7
RedMuri <celovekda@gmail.com>
2023-03-12 20:27:01 +0300

    added retrofit

---
+++ app/src/main/java/com/example/redmuriapp/retrofit/ItemsApiService.kt
@@ -0,0 +1,20 @@
+package com.example.redmuriapp.retrofit
+
+import retrofit2.http.GET
+
+interface ItemsApiService {
+
+
+    @GET(LATEST_ENDPOINT)
+    suspend fun getLatestItems()
+
+    @GET(FLASH_SALE_ENDPOINT)
+    suspend fun getFlashSaleItems()
+
+
+    companion object {
+        private const val LATEST_ENDPOINT = "cc0071a1-f06e-48fa-9e90-b1c2a61eaca7"
+        private const val FLASH_SALE_ENDPOINT = "a9ceeb6e-416d-4352-bde6-2203416576ac"
+    }
+
+}
